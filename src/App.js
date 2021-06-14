ca7fab10b560f428a92e7bfb8cf714ba262de0af
Shubham Kumar <shubham2270@gmail.com>
2021-06-14 20:50:46 +0530

    Added color scale to bars

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <LineGraph />
+      {/* <LineGraph /> */}
       <BarGraph />
     </div>
   );
--- src/BarGraph.js
+++ src/BarGraph.js
@@ -1,5 +1,12 @@
 import React, { useRef, useEffect, useState } from "react";
-import { select, axisBottom, scaleLinear, axisRight, scaleBand } from "d3";
+import {
+  select,
+  axisBottom,
+  scaleLinear,
+  axisRight,
+  scaleBand,
+  transition
+} from "d3";
 
 import "./styles.css";
 
@@ -12,18 +19,23 @@ export default function App() {
     const svg = select(svgRef.current);
 
     // Scales graph horizontly
-    const xScale = scaleLinear()
-      .domain([0, data.length - 1]) // counts of data
-      .range([0, 800]); // canvas with from 0 to max width
+    const xScale = scaleBand()
+      .domain(data.map((value, index) => index)) // counts of data
+      .range([0, 800])
+      .padding(0.5); // canvas with from 0 to max width
 
     // Scales graph vertically
     const yScale = scaleLinear()
-      .domain([0, 100]) // min to max height of graph (highest value)
+      .domain([0, 150]) // min to max height of graph (highest value)
       .range([500, 0]); // height of svg
 
+    const colorScale = scaleLinear()
+      .domain([75, 100, 150])
+      .range(["red", "orange", "green"])
+      .clamp(true);
+
     const xAxis = axisBottom(xScale) // shows downward ticks
-      .ticks(data.length) // show tick based on lenght of data
-      .tickFormat((index) => index + 1);
+      .ticks(data.length); // show tick based on lenght of data
 
     const yAxis = axisRight(yScale); // shows ticks on rightside
 
@@ -32,6 +44,20 @@ export default function App() {
 
     // selects y-axis class & shows ticks with line on right of svg
     svg.select(".y-axis").style("transform", "translateX(800px)").call(yAxis);
+
+    // Draw rect bars
+    svg
+      .selectAll(".bar")
+      .data(data)
+      .join("rect")
+      .attr("class", "bar")
+      .style("transform", "scale(1, -1)") // change bars to upside down for animations
+      .attr("x", (value, index) => xScale(index))
+      .attr("y", -500) // (-500 height of svg for putting back bars to its position for animations)
+      .attr("width", xScale.bandwidth())
+      .transition() // for animating bars, put before height
+      .attr("fill", colorScale)
+      .attr("height", (value) => 500 - yScale(value));
   }, [data]);
 
   return (
@@ -48,7 +74,7 @@ export default function App() {
         Filter Data
       </button>
       <button
-        onClick={() => setData([...data, Math.floor(Math.random() * 90 + 10)])}
+        onClick={() => setData([...data, Math.floor(Math.random() * 130 + 20)])}
       >
         Addd random number
       </button>
